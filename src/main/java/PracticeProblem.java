c30ba38c9ac86ac520c951501d4432c960352556
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-04-02 18:41:51 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,22 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int x) {
+		x = Math.abs(x);
+		if (x == 0) {
+			return x;
+		}
 
+		return x % 10 + sumOfDigits(x / 10);
+	}
+
+	public static int factorial(int n) {
+		if (n < 0) {
+			return 0;
+		}else if (n == 0) {
+			return 1;
+		}
+
+		return n * factorial(n - 1);
+	}
 }
